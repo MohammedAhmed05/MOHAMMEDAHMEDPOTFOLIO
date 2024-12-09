ce4a9001eaba48ac47b9511babf897fc38110b13
mohammed ahmed <maxedi30044@gmail.com>
2024-12-09 19:30:03 +0500

    potfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+       // JavaScript to handle form submission and show the alert
+       document.getElementById('hireMeForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevents the form from submitting
+        alert('Thank you for hiring me!');
+    });
+    alert("HI")
+confirm("WELLCOME TO MY POTFOLIO")
